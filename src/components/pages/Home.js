1867e583c3251123eb0abd99befce7bcacd22860
Apurbo Kumar Anup <apurboka@gmail.com>
2023-01-21 10:23:14 +0600

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CreateTask from "../common/CreateTask";
 import TaskList from "../common/TaskList";
 
 const Home = () => {
   return (
     <>
+      <CreateTask />
       <TaskList />
     </>
   );
